1af7fa59badd09656b6314570db6f4c400e19f96
MortezaGhazanfari <morteza.gh11@gmail.com>
2023-03-11 14:50:26 +0100

    admin attribue und Person.java

--- src/Admin.java
+++ src/Admin.java
@@ -1,2 +1,5 @@
-public class Admin {
+public class Admin extends Person {
+    public Admin(String vornaame, String nachname, int alter, String adresse, String mail) {
+        super(vornaame, nachname, alter, adresse, mail);
+    }
 }
--- src/Person.java
+++ src/Person.java
@@ -1,8 +1,15 @@
-public class Person {
+public abstract class Person {
     private String vornaame;
     private String nachname;
     private int alter;
-    private String Adresse;
+    private String adresse;
+    private String mail;
 
-
+    public Person(String vornaame, String nachname, int alter, String adresse, String mail) {
+        this.vornaame = vornaame;
+        this.nachname = nachname;
+        this.alter = alter;
+        this.adresse = adresse;
+        this.mail = mail;
+    }
 }
