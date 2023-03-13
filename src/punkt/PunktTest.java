976e648a43a1d07558e3e68c13632b62ab92b043
MortezaGhazanfari <morteza.gh11@gmail.com>
2023-03-13 08:56:35 +0100

    punkt in koordinatensystem

---
+++ src/punkt/PunktTest.java
@@ -0,0 +1,22 @@
+package punkt;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PunktTest {
+
+    @org.junit.jupiter.api.Test
+    void addierePunkte() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void subtrahierePunkte() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void multipliziereEinPunkt() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void dividiereEinPunkt() {
+    }
+}
